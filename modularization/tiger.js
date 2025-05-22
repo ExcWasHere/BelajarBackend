67cec60b6abc42fec050f12b826b52fcbfa36814
Exc <excellchriatian12@gmail.com>
2025-05-22 20:33:51 +0700

    Init Belajar Backend By Dicoding

---
+++ modularization/tiger.js
@@ -0,0 +1,11 @@
+class Tiger {
+    constructor() {
+        this.strenght = Math.floor(Math.random() * 10);
+    }
+
+    growl(){
+        console.log("Grrr!");
+    }
+}
+
+export default Tiger;
---
+++ modularization/wolf.js
@@ -0,0 +1,11 @@
+class Wolf {
+    constructor() {
+        this.strenght = Math.floor(Math.random() * 100);
+    }
+
+    howl(){
+        console.log("Owoooo!");
+    }
+}
+
+export default Wolf;
